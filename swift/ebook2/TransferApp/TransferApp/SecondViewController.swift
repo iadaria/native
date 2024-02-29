8603f4f2dc3300e7f51c07ae6c341b30787078b3
iadaria <jadarya@mail.ru>
2024-02-29 16:54:03 +0300

    work with navigation

--- swift/ebook2/TransferApp/TransferApp/SecondViewController.swift
+++ swift/ebook2/TransferApp/TransferApp/SecondViewController.swift
@@ -7,17 +7,29 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-    @IBOutlet var dataTextField: UITextField!
+protocol UpdatingDataController: AnyObject {
+    var updatingData: String { get set }
+}
+
+class SecondViewController: UIViewController, UpdatingDataController {
     
+    @IBOutlet var dataTextField: UITextField!
     var updatingData: String = ""
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        updateTextFieldData(withText: updatingData)
+        //initSceneA()
+        updateTextFieldDataA(withText: updatingData)
     }
     
-    private func updateTextFieldData(withText text: String) {
+    private func updateTextFieldDataA(withText text: String) {
         dataTextField.text = text
+    }
+    
+    @IBAction func saveDataWithProperty(_ sender: UIButton) {
+        self.navigationController?.viewControllers.forEach {
+            viewController in
+            (viewController as? UpdatingDataController)?.updatingData = dataTextField.text ?? ""
+        }
     }
 }
--- swift/ebook2/TransferApp/TransferApp/ViewController.swift
+++ swift/ebook2/TransferApp/TransferApp/ViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol UpdatableDataController: AnyObject {
+    var updatedData: String { get set }
+}
+
+class ViewController: UIViewController, UpdatableDataController {
+    var updatedData: String = "Label"
+    
     @IBOutlet var dataLabel: UILabel!
     
     override func viewDidLoad() {
@@ -17,12 +23,29 @@ class ViewController: UIViewController {
 
     @IBAction func editDataWithProperty(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let editScreen = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        let editScreen = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! UpdatingDataController
         
         editScreen.updatingData = dataLabel.text ?? ""
         
-        self.navigationController?.pushViewController(editScreen, animated: true)
+        /*let editScreenForNavigate = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController*/
+        
+        self.navigationController?.pushViewController(editScreen as! UIViewController, animated: true)
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "toEditScreen":
+            prepareEditScreen(segue)
+        default:
+            break
+        }
+    }
+    
+    private func prepareEditScreen(_ segue: UIStoryboardSegue) {
+        guard let destinationController = segue.destination as? SecondViewController else {
+            return
+        }
+        destinationController.updatingData = dataLabel.text ?? ""
+    }
 }
 
