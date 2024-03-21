9828bd9ba7add507e661a9fcc9b9a182c7afdbae
iadaria <jadarya@mail.ru>
2024-03-21 17:27:14 +0300

    _

--- swift/recall-yandex-sdk/recall-yandex-sdk/package/Private/Utilities/CryptoUtilities.swift
+++ swift/recall-yandex-sdk/recall-yandex-sdk/package/Private/Utilities/CryptoUtilities.swift
@@ -1,4 +1,7 @@
+import CommonCrypto
+import CryptoKit
 import Foundation
+
 
 enum CryptoUtilities {
     
@@ -9,5 +12,36 @@ enum CryptoUtilities {
         let base64URLEncoded = base64.reduce("") { $0 + (urlEncoding[$1] ?? String($1))}
         
         return base64URLEncoded
+    }
+    
+    static func sha256(from data: Data) -> Data {
+        if #available(iOS 13.0, *) {
+            return sha256UsingCryptoKit(from: data)
+        } else {
+            return sha256UsingCrypto(from: data)
+        }
+    }
+    
+    private static func sha256UsingCryptoKit(from data: Data) -> Data {
+        let digest = SHA256.hash(data: data)
+        var hashedData = Data(capacity: SHA256.byteCount)
+        for byte in digest {
+            hashedData.append(byte)
+        }
+        
+        return hashedData
+    }
+    
+    private static func sha256UsingCrypto(from data: Data) -> Data {
+        var digest = Array<UInt8>(repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        var hashedData = Data(capacity: Int(CC_SHA256_DIGEST_LENGTH))
+        let _ = data.withUnsafeBytes{ buffer in
+            CC_SHA256(buffer.baseAddress, UInt32(data.count), &digest)
+        }
+        for byte in digest {
+            hashedData.append(byte)
+        }
+        
+        return hashedData
     }
 }
